d52b926e89716ec79b89acc7f870d5f48884d000
Henrilsm <henri.00000848663@unicap.br>
2025-09-17 11:46:32 -0300

    alinhando bio

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Henri Moraes</h1>
-      <br></br>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>Henri Moraes</h1>
+      <br />
       <img src="/Assets/Minha foto.jpeg" width={348} height={392} />
-      <br></br>
-      <br></br>
-      <p>
+      <br />
+      <br />
+      <p style={{ textAlign: "center", maxWidth: 400 }}>
         Oi, me chamo Henri Moraes, tenho 20 anos e nasci em Salvador/BA.
-        <br></br>
-        <br></br>
+        <br />
+        <br />
         Atualmente sou estudante de ciências da computação na Universidade
         Católica de Pernambuco (UNICAP).
       </p>
